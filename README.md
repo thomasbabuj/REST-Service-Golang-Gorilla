704453f5ba896c667304c5d390c19596a91bdff5
Thomas Babu <tommy.developer@gmail.com>
2015-10-01 16:40:02 +0800

    Return some json data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # REST-Service-Golang-Gorilla
 Building a Golang REST Service with Gorilla
+
+Step 1 : Simple /hello/{name} route 
+
+Step 2 : /movies route returns some json string
+
+Step 3 : /movies/{id} -> return specific record
--- main.go
+++ main.go
@@ -7,6 +7,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,19 +15,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Movie Struct
+type Movie struct {
+	Title  string `json:"title"`
+	Rating string `json:"rating"`
+	Year   string `json:"year"`
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/hello/{name}", index).Methods("GET")
+	router.HandleFunc("/movies", handleMovies).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	log.Println("Responding to /hello request")
-	log.Println(r.UserAgent())
+func handleMovies(res http.ResponseWriter, r *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
 
-	vars := mux.Vars(r)
-	name := vars["name"]
+	var movies = map[string]*Movie{
+		"tt0076759": &Movie{Title: "Star Wars: A New Hope", Rating: "8.7", Year: "1977"},
+		"tt0082971": &Movie{Title: "Indiana Jones : Raiders of the Lost Art", Rating: "8.6", Year: "1981"},
+	}
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Hello: ", name)
+	outgoingJSON, error := json.Marshal(movies)
+
+	if error != nil {
+		log.Println(error.Error())
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintln(res, string(outgoingJSON))
 }
